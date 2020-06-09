fb6f5d65e6848e90ea85fabb1ab02b2de088bb1d
Devashish Rawat <dev.rawat7396@gmail.com>
2020-06-09 19:37:48 +0530

    added methods to BasePage

--- src/main/java/com/ag/base/BasePage.java
+++ src/main/java/com/ag/base/BasePage.java
@@ -1,7 +1,52 @@
 package com.ag.base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Created by Devashish on 08-06-2020.
  */
 public class BasePage {
+
+    WebDriver driver;
+    Properties properties;
+
+    public WebDriver initDriver(String browserName){
+
+        if(browserName.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+
+        }else if(browserName.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
+        driver.get("https://www.populusgroup.com/");
+
+        return driver;
+    }
+
+    //Reads properties from java/config
+    public Properties initProperties(){
+        properties = new Properties();
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream("\\src\\main\\java\\com\\ag\\config");
+            properties.load(inputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
 }

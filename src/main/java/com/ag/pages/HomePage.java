b7d1c1e6f9a53ec3f6525f72af36f7df6a0eb298
Devashish Rawat <dev.rawat7396@gmail.com>
2020-06-16 09:16:54 +0530

    updated git ignore

--- src/main/java/com/ag/pages/HomePage.java
+++ src/main/java/com/ag/pages/HomePage.java
@@ -1,9 +1,35 @@
 package com.ag.pages;
 
 import com.ag.base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 /**
  * Created by Devashish on 08-06-2020.
  */
 public class HomePage extends BasePage {
+
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    //Object Repo
+    By topNavEmployers = By.xpath("//a[text()='Employers']");
+    By topNavJobSeekers = By.xpath("//a[text()='JobSeekers']");
+    By topNavContractorResources = By.xpath("//a[text()='//a[text()='Contractor Resources']");
+    By topNavBlog = By.xpath("//a[text()='Blog']");
+    By topNavAbout = By.xpath("//a[text()='About']");
+
+    //Page Actions
+    public void clickTopNavItem(By locator){
+
+    }
+
+    public String getHomePageTitle(){
+
+        return driver.getTitle();
+    }
+
 }
--- src/test/java/com/ag/tests/HomePageTest.java
+++ src/test/java/com/ag/tests/HomePageTest.java
@@ -1,7 +1,38 @@
 package com.ag.tests;
 
+import com.ag.base.BasePage;
+import com.ag.pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 /**
  * Created by Devashish on 09-06-2020.
  */
 public class HomePageTest {
+
+    WebDriver driver;
+    BasePage basePage;
+    HomePage homePage;
+
+    @BeforeTest
+    public void setup(){
+
+        basePage = new BasePage();
+        driver = basePage.initDriver("firefox");
+
+        homePage = new HomePage(driver);
+    }
+
+    @Test
+    public void verifyPageTitle(){
+
+    }
+
+    @AfterTest
+    public void tearDown(){
+
+        driver.quit();
+    }
 }
